25fa3d85483d39d202b114df8d81366211461f81
hannah26hannah <ooyoonoo24@gmail.com>
2022-03-15 00:52:25 +0900

    add date format function as util

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,13 @@
+import { format, parseISO } from 'date-fns';
+
 const capitalize = (str) => {
   return str.replace(/^\w/, (c) => c.toUpperCase());
 };
 
+const formatDate = (time) => {
+  return format(parseISO(time), 'LLLL d, yyyy');
+};
+
 // 'subdays', formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true, })
 
-export { capitalize };
+export { capitalize, formatDate };
