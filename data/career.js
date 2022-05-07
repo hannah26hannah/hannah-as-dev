192dd3d8f4f908ef4b6094ad3d67aaccf7090e54
hannah26hannah <ooyoonoo24@gmail.com>
2022-05-07 16:22:58 +0900

    add resume data

---
+++ data/career.js
@@ -0,0 +1,180 @@
+const career = {
+  profile: {
+    hello: '반가워요! 서울에서 활동하는 프론트엔드 개발자, 유정원입니다.',
+    about:
+      '저는 주변 환경이 빠르게 변화할수록, 더 학습하고자 하는 동기부여를 받는 편입니다. 학습과 시행 착오는 기록으로 남겨 나와 남에게 지식 공유를 통한 더 나은 개발 문화에 기여하고자 합니다. 개발 문화, 기술의 탄생에 대한 이야기에 관심이 많고, 개발로 할 수 있는 것들을 상상하는 것을 좋아합니다. 삶과 문화의 지속 가능성을 고민하고 재미가 더해진 디지털 프로덕트를 만들고 싶습니다',
+    job: '프론트엔드 개발 Frontend development',
+    location:
+      '서울, 대한민국/원격 근무 Seoul Based, but open to remote work as well',
+  },
+  coreCompetency: [
+    {
+      title: 'Mobile App',
+      items: ['React Native, Expo를 사용한 하이브리드 모바일 앱 개발'],
+    },
+    {
+      title: 'Modern Web',
+      items: [
+        'ES 6+ 문법에 익숙한 Modern JavaScript 활용',
+        '웹 접근성을 고려한 HTML 5 마크업',
+        'CSS 3, Material UI, TailwindsCSS 등의 CSS framework 사용을 통한 반응형 레이아웃 및 컴포넌트 마크업 및 개발',
+        'React, Vue.js JavaScript Framework을 사용한 웹 개발',
+        'Next.js를 통한 정적 웹 사이트 개발',
+      ],
+    },
+    {
+      title: 'CI/CD',
+      items: [
+        'Github Actions를 통한 CI/CD workflow 구성 가능',
+        'Storybook을 통한 독립적인 환경에서의 UI component 개발/테스팅 환경 구축',
+      ],
+    },
+    {
+      title: 'Skills for the Collaboration',
+      items: [
+        'Node/Express/GraphQL 기반의 백엔드 개발 경험으로 백엔드 개발자와의 수월한 협업 추구',
+        'Figma/Adobe XD/Zeplin 등 웹 개발 디자인 툴 사용에 대한 높은 이해도',
+        'Git 사용 및 GitBucket/Github/SVN 버전 관리 시스템/툴 사용',
+        'JSDoc 등 주석과 문서화를 통해 업무 효율성을 향상 시킬 수 있음',
+        'Business Logic에 대한 이해를 바탕으로 한 기획, 디자이너, 개발 팀원과의 원활한 소통과 협의',
+        '영어를 통한 업무 가능 및 원격 근무를 하는 동료와의 능숙한 협업 가능',
+      ],
+    },
+  ],
+  careerSummary: [
+    {
+      company: '나무 AI',
+      title: 'Lead Front End Software Engineer',
+      description: 'Company Homepage 리뉴얼, Pomelo CRM/Pomelo Pop 서비스 개발',
+      usedSkills: 'React, React Native, Next, Rematch',
+      period: '2021. 10 ~ 현재',
+      detail: [
+        {
+          project: 'Company Web',
+          link: 'https://namu-ai.com/',
+          desc: ['Gatsby 기반의 홈페이지 리뉴얼', 'i18next 다국어 관리'],
+        },
+        {
+          project: 'Pomelo Pop',
+          link: '',
+          desc: [
+            '고객-중소기업 간 관계를 Play-to-Earn 방식으로 제안하는 모바일 하이브리드 웹앱으로, 고객용 웹에 참여함',
+          ],
+        },
+        {
+          project: 'Pomelo CRM',
+          link: '',
+          desc: [
+            'AI 기반의 CRM 솔루션을 제공한다.',
+            'App-Web hybrid platform을 통해 중소~대기업 비즈니스 고객을 위한 대시보드, 가상 상점 빌더 등 CRM 기능을 제공',
+          ],
+        },
+      ],
+    },
+    {
+      company: '대한항공',
+      title: 'Junior Front End Software Engineer',
+      description: 'E-document Service Platform 개발',
+      usedSkills: 'React, Material UI, Redux',
+      period: '2021.03 ~ 2021.09. 16',
+      detail: [
+        {
+          project: 'Customer Portal Web',
+          link: '',
+          desc: [
+            'React, Material UI를 활용한 각 페이지 반응형 UI 컴포넌트 및 단위 페이지 개발',
+          ],
+        },
+        {
+          project: 'Admin Web',
+          link: '',
+          desc: [
+            'Formik 등 라이브러리를 사용한 Form Validation',
+            '리액트 컴포넌트 최적화를 통한 사용자 경험 개선',
+            'redux를 사용한 상태 관리',
+          ],
+        },
+        {
+          project: 'PWA App for Crew Members',
+          link: '',
+          desc: [],
+        },
+      ],
+    },
+    {
+      company: '한화생명',
+      title: 'Junior Front End Software Engineer',
+      description: 'Coupon Mall Hybrid App/Admin Dashboard 개발',
+      usedSkills: 'Vue, vuex, Element UI',
+      period: '2020.11 ~ 2020.12',
+      detail: [
+        {
+          project: 'Coupon Mall',
+          link: '',
+          desc: [
+            '한화생명 채널 경쟁력 강화를 위한 영업지원시스템 구축 프로젝트',
+            'Vue.js, Element UI를 사용해 관리자 페이지 개발',
+            'vuex를 통한 상태 관리에 중점을 둔 Hybrid App 화면 개발',
+          ],
+        },
+        {
+          project: 'Admin Dashboard',
+          link: '',
+          desc: [
+            '한화생명 채널 경쟁력 강화를 위한 영업지원시스템 구축 프로젝트',
+            'Vue.js, Element UI를 사용해 관리자 페이지 개발',
+          ],
+        },
+      ],
+    },
+    {
+      company: 'KoopKorea',
+      title: 'Front End Software Engineer',
+      description: 'Storefront Redesign (BigCommerce)',
+      usedSkills: 'Stencil, jQuery, handlebar.js',
+      period: '2020.02 ~ 2020.04',
+      detail: [
+        {
+          project: 'Company Site',
+          link: 'https://koopkorea.nl/',
+          desc: [
+            'BigCommerce 기반의 온라인 쇼핑몰 웹사이트의 스토어프론트 리디자인 프로젝트',
+            'jQuery, handlebar.js를 이용한 반응형 웹 컴포넌트 커스텀 개발',
+            'Stencil CLI를 이용한 번들과 배포',
+          ],
+        },
+      ],
+    },
+  ],
+  education: [
+    {
+      title: '방송통신대학교 (컴퓨터과학과)',
+      period: '2021, 09 ~',
+    },
+    {
+      title:
+        '동작구 데이터 사이언스 전문가 양성과정 (Java/SQL/JSP/DataBase/bigData 학습)',
+      period: '2020.07 ~ 2020.10',
+    },
+    {
+      title: '그린컴퓨터아트학원 (JavaScript & jQuery 학습)',
+      period: '2019.12 ~ 2020.02',
+    },
+    {
+      title: '서울대학교 (독어독문학과 중퇴)',
+      period: '2013.03 ~ 2014.12',
+    },
+  ],
+  certificate: [
+    {
+      title: 'ADsP (데이터분석 준전문가) 취득',
+      date: '2020.09.29',
+    },
+    {
+      title: 'TOEIC (920) 취득',
+      date: '2020.09.13',
+    },
+  ],
+};
+
+module.exports = career;
