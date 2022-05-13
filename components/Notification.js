5193733e1439c7838b6d0df95dc95dbfb72dc07a
hannah26hannah <ooyoonoo24@gmail.com>
2022-05-13 11:49:50 +0900

    create notification bar component

---
+++ components/Notification.js
@@ -0,0 +1,14 @@
+/**
+ * @overview Notification bar on the top.
+ * Use this occasionally when you have something to notice to visitors
+ */
+
+const Notification = () => {
+  return (
+    <div className='h-10 w-full bg-purple-default flex justify-center flex-col items-center text-white'>
+      Under Construction Mode... See you soon! 🚧
+    </div>
+  );
+};
+
+export default Notification;
